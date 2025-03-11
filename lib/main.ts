8e551ea369279cc46cb709ea4fb809da5dbc4502
phil-sisu <philip.ordillo@sisugroup.com>
2025-03-11 13:14:22 +0200

    test fix

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,16 @@
-export { tsr, Config } from './tsr.js';
+import ts from 'typescript';
+import { Logger } from './util/Logger.js';
+
+export { tsr } from './tsr.js';
 export { Logger } from './util/Logger.js';
 export * from './util/error.js';
+export type Config = {
+  entrypoints: RegExp[];
+  mode: 'check' | 'write';
+  configFile?: string;
+  projectRoot?: string;
+  recursive?: boolean;
+  system?: ts.System;
+  logger?: Logger;
+  includeDts?: boolean;
+};
